ed8ae3a65edee8b712ab9451774a2d2857994b9c
Evgeny <evgenygorokhovg@yahoo.com>
2025-07-25 22:32:38 +0300

    Edited access to environment variables.

--- selfstorage/settings.py
+++ selfstorage/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
